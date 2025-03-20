a7a3d4e51371b1d3277118215f4ade1382a3eebd
Nizam <nizam@Nizams-MacBook-Pro.local>
2025-03-21 02:46:14 +0800

    added task description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,3 +17,10 @@ export default function RootLayout({
     </html>
   );
 }
+
+/**
+ * TASKS
+ * Use reset css to remove default margin, pading etc from browser
+ * Create expense calculator functionalicy
+ * Create todo list functionality
+ */
